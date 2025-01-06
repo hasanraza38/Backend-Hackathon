509ce1643631b3f4a52f351a95b9a8ae851fef72
hasanraza38 <92hasanraza689@gmail.com>
2025-01-06 21:31:56 +0500

    resolving

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import usersRoutes from "./src/routes/users.routes.js"
 import productRoutes from "./src/routes/products.routes.js"
-import ordersRoutes from "./src/routes/orders.routes.js"
+// import ordersRoutes from "./src/routes/orders.routes.js"
 
 
 const app = express()
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 // routes
 app.use("/api/v1", productRoutes)
 app.use("/api/v1/auth/", usersRoutes)
-app.use("/api/v1", ordersRoutes)
+// app.use("/api/v1", ordersRoutes)
 
 
 connectDB()
